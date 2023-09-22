b69deaf7f8a58d5565513fa535059786c7ae1c2e
noah227 <noah227@foxmail.com>
2023-09-22 15:04:37 +0800

    chore: 添加meta description

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
-    publicPath: "."
+    publicPath: ".",
+    chainWebpack(config) {
+        config.plugin("html").tap(args => {
+            args[0].title = "自动文本去重"
+            return args
+        })
+    }
 })
